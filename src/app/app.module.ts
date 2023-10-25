0f3a861c4a8d092cad39469bb1a9c7cdbff297cd
Débora <smdebora@icloud.com>
2023-10-25 20:16:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicialComponent } from './inicial/inicial.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HeaderComponent } from './header/header.component';
+import { MyProjectComponent } from './my-project/my-project.component';
+import { CommunityComponent } from './community/community.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicialComponent,
+    LoginComponent,
+    CadastroComponent,
+    HeaderComponent,
+    MyProjectComponent,
+    CommunityComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-project/my-project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-project',
+  templateUrl: './my-project.component.html',
+  styleUrls: ['./my-project.component.css']
+})
+export class MyProjectComponent {
+
+}
